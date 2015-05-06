f32af758bad8c45fc5bc219546f77b175987af50
Roope Salmi <rpsalmi@gmail.com>
2015-05-06 18:04:05 +0300

    Add editing and deleting

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,16 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
   
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    @article.update article_params
+    redirect_to @article
+  end
+  
   def create
     @article = Article.new(article_params)
     
@@ -12,6 +22,12 @@ class ArticlesController < ApplicationController
   
   def index
     @articles = Article.all
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
   
   private def article_params
