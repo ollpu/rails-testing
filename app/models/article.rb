f1807328220856f076512a30b8dc1a20b8b7a6c9
Roope Salmi <rpsalmi@gmail.com>
2015-05-08 22:34:47 +0300

    Style fiddling and creation & edit dates below articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,12 @@
 class Article < ActiveRecord::Base
+  
+  #Return a formatted date from an article
+  def get_date
+    date = created_at.to_date.strftime("Julkaistu %-d.%-m.%Y")
+    if updated_at != created_at
+      date += updated_at.to_date.strftime(" ja päivitetty viimeksi %-d.%-m.%Y.")
+    else
+      date += "."
+    end
+  end
 end
