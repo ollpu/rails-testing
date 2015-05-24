e9f6baba10a6e04c545c039976ba2638c6451a3b
Roope Salmi <rpsalmi@gmail.com>
2015-05-24 19:49:52 +0300

    Make compatible with Ruby <2.1

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -30,7 +30,8 @@ class ArticlesController < ApplicationController
     redirect_to articles_path
   end
   
-  private def article_params
+  private
+  def article_params
     params.require(:article).permit(:title, :text)
   end
 end
